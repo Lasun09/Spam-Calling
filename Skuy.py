397952a1b0be5e007d3dc8400faf6eb6aa6668f4
Lasun09 <88055505+Lasun09@users.noreply.github.com>
2021-07-28 01:52:07 +0700

    Create Skuy.py

---
+++ Skuy.py
@@ -0,0 +1,28 @@
+#!bin/bash
+clear
+echo "****************************"
+
+echo "____________________________"
+echo "PEMBUAT : Uzii/Lasun"
+echo "KONTAK  : 083100518970"
+echo "____________________________"
+echo "****************************"
+figlet Telp | lolcat
+echo "::::::::::::::::::::::::::::::"
+echo '
+[1], telepon spam
+[2], keluar &kontak admin
+'
+echo "::::::::::::::::::::::::::::::"
+echo
+read -p "masukan pilihan kalian lurd : " pil
+if [[ $pil == 1 ]]; then
+read -p "Masukan No Target contoh:89528100123  : " nomor
+link="https://id.jagreward.com/member/verify-mobile/$nomor"
+curl -s $link
+else
+echo "thanks ya lurd udah pakai tools Lasun"
+echo "kontak admin?083100518970"
+exit
+fi
+echo
